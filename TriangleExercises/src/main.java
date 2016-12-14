eb27d6b96377d054a4c66f1954696005c01230b0
李亚娥 <e199406@yahoo,com>
2016-12-14 18:06:16 +0800

    Draw a horizontal line

--- TriangleExercises/src/main.java
+++ TriangleExercises/src/main.java
@@ -1,5 +1,15 @@
+import java.util.Scanner;
+
 public class main {
     public static void main(String[] args){
-        System.out.println("*");
+        int x;
+        x = 0;
+        System.out.println("Enter a number");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        while(x<n) {
+            System.out.print("*");
+            x++;
+        }
     }
 }
