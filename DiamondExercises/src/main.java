4ca56fc19a6ffc552e786cef955a8dc676a4144d
李亚娥 <e199406@yahoo,com>
2016-12-14 18:28:02 +0800

    Isosceles Triangle

---
+++ DiamondExercises/src/main.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args){
+        int i = 0;
+        int j = 0;
+        int k = 0;
+        System.out.println("Enter a number");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        while(i<n) {
+            i++;
+            for (k=0;k<n-i;k++){
+                System.out.print(" ");
+            }
+            for(j=0;j<i*2-1;j++){
+                System.out.print("*");
+            }
+            for (k=0;k<n-i;k++){
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+}
